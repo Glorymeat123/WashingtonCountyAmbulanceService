fde3774bee01b3b6eefd34fdf29fe1cf63e54343
Glorymeat123 <joshuadarnell12@gmail.com>
2024-05-26 10:24:38 -0400

    Create scripts.js

---
+++ public/js/scripts.js
@@ -0,0 +1,22 @@
+document.getElementById('employee-login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Employee login feature is under development.');
+    document.getElementById('employee-content').style.display = 'block';
+});
+
+document.getElementById('training-request-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Training request has been sent.');
+    // Here you would add code to send the form data to an email or a backend service
+});
+
+document.getElementById('career-application-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Career application has been sent.');
+    // Here you would add code to send the form data to an email or a backend service
+});
+
+document.getElementById('director-login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Director login feature is under development.');
+    document.getElementBy
